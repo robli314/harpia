1ee4de553197b8aa606e5e7636440ebbd3697440
Rob Liebke <orobsonpires@gmail.com>
2019-08-25 23:28:56 +0200

    Refectory in login.component.ts.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,31 +21,14 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder) {
 
     // default formGroup /register
-    this.loginForm = this.fb.group({
-      'username': new FormControl('', Validators.required),
-      'email': new FormControl('', [Validators.required, Validators.email]),
-      'passwordInfo': this.fb.group({
-        'password': new FormControl('', Validators.required),
-        'confirmPassword': new FormControl('', Validators.required)
-      }, { validators: this.checkPasswords })
-    });
+    this.loginForm = this.buildFormGroup('register');
   }
 
   ngOnInit(): void {
     this.route.url.subscribe(data => {
       this.authType = data[data.length - 1].path;
-
       this.title = (this.authType === 'login') ? 'Sign in' : 'Sign up';
-
-      // we do not need email and confirmPassword in case of /login
-      if (this.authType === 'login') {
-        this.loginForm = this.fb.group({
-          'username': new FormControl('', Validators.required),
-          'passwordInfo': this.fb.group({
-            'password': new FormControl('', Validators.required),
-          })
-        });
-      }
+      this.loginForm = this.buildFormGroup(this.authType);
     });
   }
 
@@ -53,9 +36,50 @@ export class LoginComponent implements OnInit {
     console.log(this.loginForm);
   }
 
+  /**
+   * Validation function responsible for validating the insertion of new password, basically the function
+   * checks if password and confirmPassword are the same, and return a ValidationErrors accordingly.
+   * @param {FormGroup} group - to be validated.
+   * @returns {ValidationErrors} - validation error result.
+   * @memberof LoginComponent
+   */
   checkPasswords(group: FormGroup): ValidationErrors {
     let password = group.get('password').value;;
     let confirmPassword = group.get('confirmPassword').value;
     return password === confirmPassword ? null : { notSame: true }
+  }
+
+  /**
+   * Function responsible for building and returning a FormGroup based on authType.
+   * @param {String} authType - authentication type.
+   * @returns {FormGroup} - form group result.
+   * @memberof LoginComponent
+   */
+  buildFormGroup(authType: String): FormGroup {
+
+    let formGroup: FormGroup;
+
+    switch (authType) {
+      case 'login': {
+        formGroup = this.fb.group({
+          'username': new FormControl('', Validators.required),
+          'email': new FormControl('', [Validators.required, Validators.email]),
+          'passwordInfo': this.fb.group({
+            'password': new FormControl('', Validators.required),
+            'confirmPassword': new FormControl('', Validators.required)
+          }, { validators: this.checkPasswords })
+        });
+        break;
+      } default: {
+        // we do not need email and confirmPassword in case of /login
+        formGroup = this.loginForm = this.fb.group({
+          'username': new FormControl('', Validators.required),
+          'passwordInfo': this.fb.group({
+            'password': new FormControl('', Validators.required),
+          })
+        });
+      }
+    }
+    return formGroup;
   }
 }
