459cd9e8cb73549b977cb6866b7ce2e0079c1232
None <orobsonpires@gmail.com>
2019-09-03 23:01:54 +0200

    Remove selector from dialognotification component.

--- src/app/shared/notification-dialog/notification-dialog.component.ts
+++ src/app/shared/notification-dialog/notification-dialog.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-  selector: 'app-root',
   templateUrl: './notification-dialog.component.html',
   styleUrls: ['./notification-dialog.component.scss']
 })
