f8eeef4db0886ed71b60a3504ad30127cb08680d
Rob Liebke <orobsonpires@gmail.com>
2019-09-09 22:04:27 +0200

    Remove common module import.

--- src/app/alert/alert.module.ts
+++ src/app/alert/alert.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ModalAlertComponent } from './modal-alert/modal-alert.component';
@@ -6,7 +5,6 @@ import { ModalAlertComponent } from './modal-alert/modal-alert.component';
 @NgModule({
   declarations: [ModalAlertComponent],
   imports: [
-    CommonModule,
     SharedModule
   ]
 })
