871289d6209896ad7ecd33b4d9a8dccee8a8311b
Rob Liebke <orobsonpires@gmail.com>
2019-09-11 21:55:07 +0200

    Add auth guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
     path: 'pages',
-    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
-  }
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), // dynamically loading
+    canActivate: [AuthGuard]
+  },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
 ];
 
 @NgModule({
