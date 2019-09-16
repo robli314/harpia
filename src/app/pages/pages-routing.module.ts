e94b9da92c654c9effc91b7a5aca884ba6130b61
None <orobsonpires@gmail.com>
2019-09-16 22:30:32 +0200

    Replace hom component by dashboard.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesAuthResolver } from './pages-auth.resolver';
 
 const routes: Routes = [{
     path: '',
-    component: HomeComponent,
+    component: DashboardComponent,
     resolve: {
         isAuthenticated: PagesAuthResolver
     },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesAuthResolver } from './pages-auth.resolver';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [DashboardComponent],
   imports: [
     PagesRoutingModule,
     SharedModule
