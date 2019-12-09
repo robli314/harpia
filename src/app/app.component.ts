2b115a290a79e3bf738ae0de2f17223176ca92e2
None <orobsonpires@gmail.com>
2019-12-09 20:49:36 +0100

    Remove old logging strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserService } from './services/user.service';
 
@@ -9,7 +9,10 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent implements OnInit {
   isAuthenticated: Observable<boolean>;
-  constructor(private _userService: UserService) { }
+  constructor(private _userService: UserService,
+    @Inject(LOCALE_ID) private _locale: string) {
+    console.log(this._locale)
+  }
 
   ngOnInit() {
     this.isAuthenticated = this._userService.isAuthenticated;
--- src/app/helpers/custom-error-handler.ts
+++ src/app/helpers/custom-error-handler.ts
@@ -1,6 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Injectable, Injector } from "@angular/core";
-import { LoggingService } from '../services/logging.service';
 
 @Injectable()
 export class CustomErrorHandler implements ErrorHandler {
@@ -8,6 +7,6 @@ export class CustomErrorHandler implements ErrorHandler {
     // I need to use the injector because the ErrorHandler is created before the providers.
     constructor(private _injector: Injector) { }
     handleError(error: Error | HttpErrorResponse) {
-        this._injector.get(LoggingService).error(error);
+        //this._injector.get(LoggingService).error(error);
     }
 }
